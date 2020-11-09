2b0683fcb6d7d69c5a60ea856c2987d03b612731
Okechukwu O Uneze <unezeo@gmail.com>
2020-11-09 05:51:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,21 +16,37 @@ end
 def get_japanese_emoticon(path, emoticon)
   # code goes here
   emoji = load_library(path)
+<<<<<<< HEAD
   emoji.map do |name, value|
     if emoji[name].has_value?(emoticon)
       return emoji[name][:japanese]
     end
   end
   return "Sorry, that emoticon was not found"
+=======
+  # if emoji[name].has_key?(emoticon)
+  #   return emoji[name][:japanese]
+  # else
+  #   return "No key here"
+  # end
+>>>>>>> 5eba7111a10d34312a8a67ce0f98f48fdffd8783
 end
 
 def get_english_meaning(path, emoticon)
   # code goes here
   emoji = load_library(path)
+<<<<<<< HEAD
   emoji.each do |name, value|
     if emoji[name].has_value?(emoticon)
       return emoji.index(value)
     end
   end
   return "Sorry, that emoticon was not found"
+=======
+  # if emoji[name].has_key?(emoticon)
+  #   return emoji.index(emoticon)
+  # else
+  #   return "No key here"
+  # end
+>>>>>>> 5eba7111a10d34312a8a67ce0f98f48fdffd8783
 end
